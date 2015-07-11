a05f473dd6f4fe3ae606732551f3964bd7acb5f7
Andrey Kravtsov <raidendev@gmail.com>
2015-07-11 18:45:31 +0300

    Refactor test helpers/var

--- tests/helpers/var.js
+++ tests/helpers/var.js
@@ -1,21 +1,17 @@
 /*jshint maxlen:false*/
-var Handlebars = require('handlebars'),
+
+var should = require('should'),
+    Handlebars = require('handlebars'),
     varHelper = require('../../helpers/var');
 
 describe('Built-in helpers', function () {
-  var handlebars = null;
-
-  beforeEach(function () {
-    handlebars = Handlebars.create();
-    varHelper(handlebars);
-  });
 
   describe('var', function () {
+    var handlebars = Handlebars.create();
+    varHelper(handlebars);
 
     it('should convert minuses to undescores', function () {
-      if (handlebars.helpers.var('foo-bar') !== 'foo_bar') {
-        throw new Error('unexpected convert result');
-      }
+      handlebars.helpers.var('foo-bar').should.equal('foo_bar');
     });
 
   });
